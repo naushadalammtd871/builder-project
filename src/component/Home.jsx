e2cad6f6ca7b07c3bbdb3f2724f03a140f0c8e60
naushadalammtd871 <naushadalammtd871@gmail.com>
2025-10-01 11:52:05 +0530

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom'
 const Home = () => {
     const navigate = useNavigate();
   return (
-    <div className='flex items-center mt-26 sm:mt-40 justify-center flex-col gap-10'>
+    <div className='flex items-center mt-30 sm:mt-40 justify-center flex-col gap-10'>
         <motion.div initial={{opacity: 0, y: -500}} animate={{opacity: 1, y: 0}} transition={{duration: 2}} className="">
             <h1 className='text-emerald-600 sm:text-3xl text-2xl font-bold'>Hello developer</h1>
         </motion.div>
@@ -15,7 +15,7 @@ const Home = () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus consequuntur repellendus vitae laboriosam sed nemo incidunt, distinctio reprehenderit tempore sint iste accusamus vero est quos error officia illum ipsam odio quod hic nihil eius expedita nulla!
             </p>
         </motion.div>
-        <motion.button onClick={() => navigate("/lowerpart")} initial={{opacity: 0, x: 500}} animate={{opacity: 1, x: 0}} transition={{duration: 2}} className='text-xl font-bold text-[#333333] bg-emerald-600 py-[10px] px-[30px] rounded-full'>Explore</motion.button>
+        <motion.button onClick={() => navigate("/lowerpart")} initial={{opacity: 0, x: 500}} animate={{opacity: 1, x: 0}} transition={{duration: 2}} className='text-xl font-bold text-[#333333] bg-emerald-600 py-[10px] mt-4 px-[30px] rounded-full'>Explore</motion.button>
     </div>
   )
 }
